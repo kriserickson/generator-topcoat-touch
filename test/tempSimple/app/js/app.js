284ebdb7cb1ed8365874c2acb723290a3b5775a6
ksoncan34 <ksoncan34@yahoo.ca>
2014-05-18 13:07:01 -0700

    Fix issue #2 Add tests for cordova.

---
+++ test/tempSimple/app/js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Demo App for TopcoatTouch
+ */
+$(document).ready(function() {
+
+
+    // Create the topcoatTouch object
+    var tt = new TopcoatTouch();
+
+    // First page we go to home...  This could be done in code by setting the class to 'page page-center', but here is how to do it in code...
+
+    tt.goTo('home');
+
+
+
+});
